eb7e3f7b3c80b39fca9b02845f940b89cc7b7327
Hugh Kennedy <hughskennedy@gmail.com>
2014-02-22 15:45:55 +1100

    Prevent errors when passed empty modules in bundle.
    If the function body is empty, a `range` value will not be
    defined on the body and an error will be thrown trying to
    access its properties.

--- test/unpack.js
+++ test/unpack.js
@@ -6,6 +6,7 @@ var vm = require('vm');
 
 var fs = require('fs');
 var src = fs.readFileSync(__dirname + '/files/bundle.js', 'utf8');
+var empty = fs.readFileSync(__dirname + '/files/empty.js', 'utf8');
 
 test('unpack', function (t) {
     t.plan(1);
@@ -23,3 +24,20 @@ test('unpack', function (t) {
     rows.forEach(function (row) { p.write(row) });
     p.end();
 });
+
+test('does not break on empty modules', function (t) {
+    t.plan(1);
+
+    var p = pack({ raw: true });
+    var rows = unpack(empty);
+
+    p.pipe(concat(function (body) {
+        t.pass('did not emit an error');
+    }))
+
+    rows.forEach(function (row) {
+        p.write(row);
+    })
+
+    p.end();
+})
