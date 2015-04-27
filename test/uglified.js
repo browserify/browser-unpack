032c4892e1f8808868f11d9a7cee593c5ff37b32
mattdesl <dave.des@gmail.com>
2015-04-26 21:43:25 -0400

    fix case where uglify turns strings into identifiers

--- test/uglified.js
+++ test/uglified.js
@@ -8,10 +8,19 @@ var fs = require('fs');
 var src = fs.readFileSync(__dirname + '/files/uglified.js', 'utf8');
 
 test('uglified', function (t) {
-    t.plan(2);
+    t.plan(3);
     t.doesNotThrow(function() {
         var p = pack({ raw: true });
         var rows = unpack(src);
         t.equal(rows.length, 3, 'should unpack 3 rows');
     }, 'should not throw');
+    
+    t.deepEquals(deps(src), ['./foo.js', 'gamma']);
+
+    function deps(src) {
+      var rows = unpack(src);
+      return rows.reduce(function(prev, row) {
+        return prev.concat(Object.keys(row.deps));
+      }, []);
+    }
 });
