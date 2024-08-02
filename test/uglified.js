2c4e86cc3155ffcefa5317777b762497bd3871e3
p0ise <changelf@163.com>
2024-08-03 02:33:50 +0800

    fix case where uglify turns `file.key` into identifiers when using alias
    When using alias with browserify, uglify may transform`file.key` into identifiers, leading to missing `row.id`. This commit adds a fix to ensure that file.key is properly handled in such cases.

--- test/uglified.js
+++ test/uglified.js
@@ -8,11 +8,12 @@ var fs = require('fs');
 var src = fs.readFileSync(__dirname + '/files/uglified.js', 'utf8');
 
 test('uglified', function (t) {
-    t.plan(3);
+    t.plan(4);
     t.doesNotThrow(function() {
         var p = pack({ raw: true });
         var rows = unpack(src);
         t.equal(rows.length, 3, 'should unpack 3 rows');
+        t.ok(rows[2].hasOwnProperty('id'), 'third row should have an id property');
     }, 'should not throw');
     
     t.deepEquals(deps(src), ['./foo.js', 'gamma']);
